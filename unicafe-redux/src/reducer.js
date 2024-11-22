8e58aaaaa0c5f379a9136000d27729d9a09bd3e9
Javier Rosales <cyjav.dev@gmail.com>
2024-11-22 15:06:46 -0600

    Make counterReducer to work and add tests

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -4,17 +4,27 @@ const initialState = {
   bad: 0
 }
 
-const counterReducer = (state=initialState, action) {
-  console.log(action)
+const counterReducer = (state=initialState, action) => {
   switch (action.type) {
     case 'GOOD':
-      return state
+      return {
+        ...state,
+        good: state.good + 1
+      }
     case 'OK':
-      return state
+      return {
+        ...state,
+        ok: state.ok + 1
+      }
     case 'BAD':
-      return state
+      return {
+        ...state,
+        bad: state.bad + 1
+      }
     case 'ZERO':
-      return state
+      return {
+        ...initialState
+      }
     default:
       return state
   }
