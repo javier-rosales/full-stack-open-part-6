defdf2645f1b6d70657a90400cf50de1f1ecc184
Javier Rosales <cyjav.dev@gmail.com>
2024-11-25 14:22:58 -0600

    Fix slice name

--- anecdotes-redux/src/reducers/filterReducer.js
+++ anecdotes-redux/src/reducers/filterReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const filterSlice = createSlice({
-  name: 'anecdotes',
+  name: 'filter',
   initialState: '',
   reducers: {
     filterChange(state, action) {
